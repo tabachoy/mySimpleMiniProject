a0245f8bc79035408b37f5ab4dc0650b767a694d
Emmanuel Rebellon <emman.rebellon@gmail.com>
2023-09-12 22:52:50 +0800

    test check out branch and pust cicd 2nd try s3 backennd change

--- tf-cicd-run/main.tf
+++ tf-cicd-run/main.tf
@@ -15,7 +15,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "sctp-ce3-tfstate-bucket"
+    bucket = "sctp-ce3-tfstate-bucket-1"
     region = "us-east-1"
     key    = "emman.tfstate"
   }
